824181f48a1650f53fe2e535ab6cbe38b6b84741
TJoeMoran <tjmoran@nic.edu>
2025-05-14 14:37:47 -0700

    Add files via upload

--- FinalCalculator.asm
+++ FinalCalculator.asm
@@ -1,7 +1,9 @@
 ;****************************************************************
 ; Name: Joseph Moran
 ; Date: 4/28/2025
-; Description: 
+; Description: Basic calculator that performs addition, subtraction,
+;              multiplication, and integer division on two-digit
+;              values (00 - 99). 
 ;
 ; Register Usage:
 ;   R0 - reserved for TRAP output
@@ -15,16 +17,25 @@
 ;****************************************************************
         .ORIG x3000
         
-        ;initialize stack pointer
-        LD R6,Stack
+;starting strings        
+;****************************************************************        
+WELCOME         .STRINGZ "\nWelcome to the LC-3 Calculator!"
+INPUT_PROMPT    .STRINGZ "\nEnter two numbers between 00 and 99 separated by an operator ('+', '-', '*', or '/') or press \"enter\" to exit: "
+;****************************************************************
         
-        ;get first two digits
-        ;get operator
-        ;get 2nd two digits
+        ;initialize stack pointer
+        LD R6,STACK
+        
+        ;welcome message
+        LEA R0,WELCOME
+        PUTS
+START
         LEA R0,INPUT_PROMPT
         PUTS
 GET_INPUT
         JSR getOperand
+        ADD R0,R1,#2
+        BRz EXIT
         ADD R2,R1,#0
         BRn REDO
         JSR getOperator
@@ -46,7 +57,9 @@ NEXT    ADD R4,R4,#-1
         BRz MULTIPLICATION
         
         JSR DIVIDE
-        BR CONVERT
+        ADD R1,R1,#0
+        BRzp CONVERT
+        BRn DIVIDE_0
 ADDITION
         JSR SUM
         BR CONVERT
@@ -64,15 +77,22 @@ DISPLAY_RESULT
         PUTS
         ADD R0,R1,#0
         PUTS
-
+        BR START
         
+DIVIDE_0
+        LEA R0,DIV_0_ERROR
+        PUTS
+        BR START
         
-EXIT    HALT
+EXIT    LEA R0, EXIT_STR
+        PUTS
+        HALT
 ;****************************************************************
-Stack           .FILL xEFFF
-Error           .STRINGZ "\nInvalid input! Please enter two numbers between 00 and 99 separated by an operator ('+', '-', '*', or '/'): "
-INPUT_PROMPT    .STRINGZ "\nWelcome to the LC-3 Calculator! \nEnter two numbers between 00 and 99 separated by an operator ('+', '-', '*', or '/'): "
+STACK           .FILL xEFFF
+ERROR           .STRINGZ "\nInvalid input! Please enter two numbers between 00 and 99 separated by an operator ('+', '-', '*', or '/') or press \"enter\" to exit: "
 RESULT_STR      .STRINGZ "\nResult of calculation: "
+DIV_0_ERROR     .STRINGZ "\nCannot divide by 0!"
+EXIT_STR        .STRINGZ "\nExiting calculator... "
 
 
 
@@ -100,6 +120,10 @@ getDigit
             GETC
             OUT
             
+            ;check for newline character
+            ADD R2,R0,#-10
+            BRz ENTER
+            
             ;check range
             LD R2,UPPER_RANGE
             ADD R2,R0,R2
@@ -111,6 +135,10 @@ getDigit
 OUT_OF_RANGE
             AND R1,R1,#0
             ADD R1,R1,#-1 ;return -1
+            BR DONE
+            
+ENTER       AND R1,R1,#0
+            ADD R1,R1,#-2 ;return -2
             
             ;load registers
 DONE        LDR R2,R6,#0
@@ -223,6 +251,9 @@ getOperand
 
             ;get tens value
             JSR getDigit
+            ADD R2,R1,#2
+            BRz ENTER_EXIT
+            AND R2,R2,#0 ;clear R2
             ADD R1,R1,#0
             BRn OUT_OF_RANGE3
 TENS        ADD R2,R1,R2    ;multiply by 10    
@@ -237,7 +268,10 @@ TENS        ADD R2,R1,R2    ;multiply by 10
             
 OUT_OF_RANGE3
             AND R1,R1,#0
-            ADD R1,R1,#-1 ;return -1            
+            ADD R1,R1,#-1 ;return -1     
+            
+ENTER_EXIT  AND R1,R1,#0
+            ADD R1,R1,#-2 ;return -2
             
             ;load registers
 DONE3       LDR R4,R6,#0
@@ -249,165 +283,6 @@ DONE3       LDR R4,R6,#0
 
             RET
 ;****************************************************************
-
-
-
-;*********************decToFloat*********************************
-; Description: takes a decimal value and converts it to fixed-
-;              point (Q8.8) number format
-;
-; Register Usage:
-;   R1 - return value
-;   R2 - input value/used for calculation
-;   R4 - loop counter
-;   R7 - reserved for RET
-;****************************************************************
-decToFloat
-            ;save registers
-            ADD R6,R6,#-1
-            STR R7,R6,#0
-            ADD R6,R6,#-1
-            STR R2,R6,#0
-            ADD R6,R6,#-1
-            STR R4,R6,#0
-            
-            LD R4,DEC_TO_FLOAT
-LOOP        ADD R2,R2,R2
-            ADD R4,R4,#-1
-            BRp LOOP
-            ADD R1,R2,#0
-            
-            ;load registers
-            LDR R4,R6,#0
-            ADD R6,R6,#1
-            LDR R2,R6,#0
-            ADD R6,R6,#1
-            LDR R7,R6,#0
-            ADD R6,R6,#1
-            
-            RET
-;****************************************************************
-DEC_TO_FLOAT .FILL #8
-
-
-
-;*********************floatToDec*********************************
-; Description: takes a Q8.8 fixed point value and converts it
-;              to standard decimal
-;
-; Register Usage:
-;   R1 - returns starting address of string/used to store decimal portion
-;   R2 - input value/used for calculation
-;   R3 - used for calculation/memory location in string storage
-;   R4 - integer portion
-;   R7 - reserved for RET
-;****************************************************************
-floatToDec
-            ;save registers
-            ADD R6,R6,#-1
-            STR R7,R6,#0
-            ADD R6,R6,#-1
-            STR R0,R6,#0
-            ADD R6,R6,#-1
-            STR R2,R6,#0
-            ADD R6,R6,#-1
-            STR R3,R6,#0
-            ADD R6,R6,#-1
-            STR R4,R6,#0
-            ADD R6,R6,#-1
-            STR R5,R6,#0
-            
-            ;get integer portion (divide by 256 to shift R2 >> 8)
-            LD R3,DIV_256
-            JSR divideFloat ;now R1 holds integer portion
-            ADD R4,R1,#0    ;move to R4
-            
-            ;get decimal portion
-            LD R3,FRACTION_MASK
-            AND R2,R2,R3    ;now R2 holds fractional portion in Q8.8
-            LD R3,HUNDRED
-            ;JSR multiplyFloat ;now R1 holds (fractional Q8.8) * 100
-            ADD R2,R1,#0
-            LD R3,DIV_256
-            JSR divideFloat   ;now R1 holds decimal number representing fractional portion (0-99)
-            
-            ;store integer digit in string
-NEXT_STEP3  ADD R2,R4,#0
-            JSR divide10 ;R2 holds quotient, R3 holds remainder
-            ADD R6,R6,#-1
-            STR R3,R6,#0
-            ADD R4,R4,#1
-            ADD R0,R2,#-10
-            BRzp NEXT_STEP3
-            
-            LD R1,DEC_TO_ASCII
-            ADD R2,R2,#0
-            BRz STR_LOOP
-            ADD R2,R1,R2 ;convert to ASCII
-            STR R2,R5,#0 ;store first digit in string
-            ADD R5,R5,#1
-            
-STR_LOOP3   
-            LDR R2,R6,#0
-            ADD R6,R6,#1
-            ADD R2,R1,R2 ;convert to ASCII
-            STR R2,R5,#0 ;store next digit in string
-            ADD R5,R5,#1
-            ADD R4,R4,#-1
-            BRp STR_LOOP3
-            LEA R3,OUTPUT_STRING
-            LD R2,DEC_TO_AS
-            ADD R4,R2,R4
-            STR R4,R3,#0 ;store integer digit
-            ADD R3,R3,#1
-            
-            ;store '.' character
-            LD R2,DOT
-            STR R2,R3,#0
-            ADD R3,R3,#1
-            
-            ;store tenths place digit
-            AND R2,R2,#0
-            ADD R2,R2,#-10 ;divisor
-            AND R4,R4,#0   ;loop counter
-TENTHS      ADD R4,R4,#1
-            ADD R1,R1,R2
-            BRzp TENTHS
-            ADD R1,R1,#10  ;undo extra subtraction - R1 now contains hundredths digit
-            ADD R4,R4,#-1  ;R4 now contains tenths digit
-            STR R4,R3,#0
-            ADD R3,R3,#1
-            
-            ;store hundredths place digit and null terminator
-            STR R1,R3,#0
-            AND R2,R2,#0
-            STR R2,R3,#1
-            
-            ;return starting address of output string
-            LEA R1,OUTPUT_STRING
-            
-            ;load registers
-            LDR R5,R6,#0
-            ADD R6,R6,#1
-            LDR R4,R6,#0
-            ADD R6,R6,#1
-            LDR R3,R6,#0
-            ADD R6,R6,#1
-            LDR R2,R6,#0
-            ADD R6,R6,#1
-            LDR R0,R6,#0
-            ADD R6,R6,#1
-            LDR R7,R6,#0
-            ADD R6,R6,#1
-            
-            RET
-;****************************************************************
-DIV_256         .FILL #256
-FRACTION_MASK   .FILL x00FF
-HUNDRED         .FILL #100  ;gives precision to hundredths place
-OUTPUT_STRING   .BLKW #6    ;holds output string
-DEC_TO_AS       .FILL #48
-DOT             .FILL #46   ;ASCII code for dot
 
 
 
@@ -430,14 +305,19 @@ MULT
             STR R3,R6,#0
             
             AND R1,R1,#0
+            ;check if either value is zero
+            ADD R2,R2,#0 
+            BRz ZERO
+            ADD R3,R3,#0
+            BRz ZERO
+            
             ;multiply
-
 MULT_LOOP
             ADD R1,R1,R2
             ADD R3,R3,#-1
             BRp MULT_LOOP
             
-            ;load registers
+ZERO        ;load registers
             LDR R3,R6,#0
             ADD R6,R6,#1
             LDR R2,R6,#0
@@ -639,51 +519,6 @@ FINISH      LDR R4,R6,#0
 
 
 
-;*********************divideFloat********************************
-; Description: returns the quotient of two Q8.8 values
-;
-; Register Usage:
-;   R1 - return value
-;   R2 - numerator (Q8.8)
-;   R3 - denominator (Q8.8)
-;   R4 - used for subtraction in loop
-;   R7 - reserved for RET
-;****************************************************************
-divideFloat
-            ;save registers
-            ADD R6,R6,#-1
-            STR R7,R6,#0
-            ADD R6,R6,#-1
-            STR R2,R6,#0
-            ADD R6,R6,#-1
-            STR R3,R6,#0
-            ADD R6,R6,#-1
-            STR R4,R6,#0
-            
-            JSR decToFloat ;multiplies numerator*256 - now numerator is in R1
-            NOT R4,R3
-            ADD R4,R4,#1   ;make denominator negative
-            AND R2,R2,#0   ;use R2 as counter
-DIV256      ADD R2,R2,#1
-            ADD R1,R1,R4
-            BRzp DIV256
-            ADD R1,R2,#-1  ;remove extra increment and store in R1
-            
-            ;load registers
-            LDR R4,R6,#0
-            ADD R6,R6,#1
-            LDR R3,R6,#0
-            ADD R6,R6,#1
-            LDR R2,R6,#0
-            ADD R6,R6,#1
-            LDR R7,R6,#0
-            ADD R6,R6,#1
-            
-            RET
-;****************************************************************
-
-
-
 ;*********************DIVIDE*************************************
 ; Description: divides two integers and returns a string with
 ;              the answer as a decimal
@@ -706,78 +541,24 @@ DIVIDE
             ADD R6,R6,#-1
             STR R5,R6,#0
             
-            NOT R4,R3
+            ADD R3,R3,#0 ;check for divide by 0 error
+            BRp CONTINUE
+            AND R1,R1,#0
+            ADD R1,R1,#-1
+            ;LEA R1,DIV_0_ERROR
+            BR RETURN
+            
+CONTINUE    NOT R4,R3
             ADD R4,R4,#1 ;flip R3 and store in R4
-            ADD R0,R4,#0 ;store copy
             AND R5,R5,#0 ;loop counter
 DIV_LOOP    ADD R5,R5,#1
             ADD R2,R2,R4
-            BRz STORE_INTEGER
-            BRp DIV_LOOP
+            BRzp DIV_LOOP
             
             ADD R1,R5,#-1 ;return quotient in R1
             
-            
-            
-            ;ADD R3,R2,#0  ;R3 holds remainder
-            
-STORE_INTEGER
-            ;AND R4,R4,#0
-            ;ADD R2,R5,#0
-NEXT_STEP2  ;JSR divide10 ;R2 holds quotient, R3 holds remainder
-            ;ADD R6,R6,#-1
-            ;STR R3,R6,#0
-            ;ADD R4,R4,#1
-            ;ADD R1,R2,#-10
-            ;BRzp NEXT_STEP2
-            
-            
-            ;LEA R5,DIVIDE_STR
-            ;ADD R2,R2,#0
-            ;BRz STR_LOOP2
-            ;LD R1,DEC_TO_ASCII
-            ;ADD R2,R1,R2 ;convert to ASCII
-            ;STR R2,R5,#0 ;store first digit in string
-            ;ADD R5,R5,#1
-            
-STR_LOOP2   ;LD R1,DEC_TO_ASCII
-            ;LDR R2,R6,#0
-            ;ADD R6,R6,#1
-            ;ADD R2,R1,R2 ;convert to ASCII
-            ;STR R2,R5,#0 ;store next digit in string
-            ;ADD R5,R5,#1
-            ;ADD R4,R4,#-1
-            ;BRp STR_LOOP2
-            
-            ;ADD R2,R3,#0
-            ;JSR decToFloat
-            ;ADD R3,R1,#0 ;R3 holds remainder as Q8.8 (R0 holds flipped denominator)
-            ;ADD R2,R5,#0
-            ;JSR decToFloat ;R1 holds quotient as Q8.8
-            ;ADD R2,R1,R3
-            ;JSR floatToDec
-            
-            
-            
-            
-            ;AND R3,R3,#0 ;loop counter
-DIV_LOOP2   ;ADD R3,R3,#1
-            ;ADD R2,R2,R0
-            ;BRzp DIV_LOOP2
-            
-            ;ADD R2,R3,#-1   ;R2 now holds fractional portion in Q8.8
-            ;JSR floatToDec  ;R1 holds pointer to fractional portion string
-            ;ADD R3,R1,#0
-            ;ADD R2,R5,#0
-            ;JSR decToFloat
-            ;ADD R2,R1,#0
-            ;JSR 
-            
-            
-            ;LEA R1,DIVIDE_STR ;return pointer to string
-            
             ;load registers
-            LDR R5,R6,#0
+RETURN      LDR R5,R6,#0
             ADD R6,R6,#1
             LDR R4,R6,#0
             ADD R6,R6,#1
@@ -788,10 +569,6 @@ DIV_LOOP2   ;ADD R3,R3,#1
             
             RET
 ;****************************************************************
-PI_APPROX   .FILL #804 ;pi converted to Q8.8
-NUM#180     .FILL #180
-NUM#256     .FILL #256
-DIVIDE_STR  .BLKW #6
 
 
 
